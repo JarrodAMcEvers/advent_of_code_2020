555cfbb89c2e1930fe4c2d970380b97819e25018
Jarrod McEvers <jarrodamcevers@gmail.com>
2020-12-19 10:49:42 -0600

    Refactor part1.

--- day19/day19.py
+++ day19/day19.py
@@ -5,12 +5,12 @@ file = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
 with open(file, 'r') as f:
     rules, lines = [ line.strip() for line in f.read().split('\n\n') ]
 
-MAX_DEPTH = 6
+MAX_DEPTH = 5
 lines = lines.split('\n')
 rules = { int(rule.split(':')[0]): rule.split(':')[1].strip() for rule in rules.split('\n') }
 solved = {}
 
-def solve(rule_number, depth=0) -> str:
+def solve_part1(rule_number) -> str:
     rule_number = int(rule_number)
     item = rules[rule_number]
     temp = '('
@@ -22,17 +22,18 @@ def solve(rule_number, depth=0) -> str:
             nums1 = nums1.strip()
             nums2 = nums2.strip()
             for num in nums1.split(' '):
-                temp += solve(num)
+                temp += solve_part1(num)
             temp += '|'
             for num in nums2.split(' '):
-                temp += solve(num)
+                temp += solve_part1(num)
             return temp + ')'
         else:
             for number in item.split(' '):
-                temp += solve(number)
+                temp += solve_part1(number)
             return temp + ')'
-solved[0] = solve(0)
+def part1():
+    solved[0] = solve_part1(0)
+    regex = '^{}$'.format(solved[0])
+    return len(list(filter(lambda x: re.match(regex, x), lines)))
 
-regex = '^' + solved[0] + '$'
-part1 = len([1 for item in filter(lambda x: re.match(regex, x), lines)])
-print('Part 1:', part1)
+print('Part 1:', part1())
